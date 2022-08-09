e1d92b16e9b52e5656a003e2e95672366793e158
techchoi <ninahjc.94@gmail.com>
2022-08-09 01:27:54 -0300

    commit inicial

---
+++ src/main/java/nina/choi/calcRest/CalcRestApplication.java
@@ -0,0 +1,13 @@
+package nina.choi.calcRest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalcRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalcRestApplication.class, args);
+	}
+
+}
